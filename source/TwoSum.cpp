17d739e90a1c7e6eb2d29773c2e1f0d9dfbf69f9
Nicolae Christoffersen <nicochristof@gmail.com>
2026-04-09 18:01:58 -0700

    Two Sum

---
+++ source/TwoSum.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+
+#include <unordered_map>
+
+// One pass hashMap solution
+class Solution
+{
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
+    {
+        std::unordered_map<int, int> hashMap; // value, index
+        int complement = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            complement = target - nums[i];
+            if (hashMap.contains(complement))
+            {
+                return std::vector<int>({ i, hashMap[complement] });
+            }
+            hashMap[nums[i]] = i;
+        }
+        return std::vector<int>();
+    }
+};
+
+//// Two pass hashmap solution
+//class Solution
+//{
+//public:
+//    std::vector<int> twoSum(std::vector<int>& nums, int target)
+//    {
+//        std::unordered_map<int, int> hashMap; // value, index
+//        for (int i = 0; i < nums.size(); ++i)
+//        {
+//            hashMap[nums[i]] = i;
+//        }
+//
+//        int complement = 0;
+//        for (int i = 0; i < nums.size(); ++i)
+//        {
+//            complement = target - nums[i];
+//            if (hashMap.contains(complement) && hashMap[complement] != i)
+//            {
+//                return std::vector<int>({ i, hashMap[complement] });
+//            }
+//        }
+//        return std::vector<int>();
+//    }
+//};
