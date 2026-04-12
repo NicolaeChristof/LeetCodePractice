0e4918f5ab74b4d31beb8f60c9a7a93d81ca10a6
Nicolae Christoffersen <nicochristof@gmail.com>
2026-04-12 14:29:50 -0700

    Best Time to Buy and Sell Stock

---
+++ source/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Solution
+{
+public:
+    int maxProfit(std::vector<int>& prices)
+    {
+        if (prices.empty()) return 0;
+
+        int lowestPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.size(); ++i)
+        {
+            if (prices[i] < lowestPrice)
+            {
+                lowestPrice = prices[i];
+                continue;
+            }
+
+            if (prices[i] - lowestPrice > maxProfit)
+            {
+                maxProfit = prices[i] - lowestPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+};
