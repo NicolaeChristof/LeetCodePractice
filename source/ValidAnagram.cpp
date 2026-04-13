487f94f3ff092e36eeeed77e3578adce21c34a20
Nicolae Christoffersen <nicochristof@gmail.com>
2026-04-12 17:06:39 -0700

    added links to problems at top of source files

--- source/ValidAnagram.cpp
+++ source/ValidAnagram.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/valid-anagram
 #include <string>
 
 #include <unordered_map>
--- source/ValidPalindrome.cpp
+++ source/ValidPalindrome.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/valid-palindrome
 #include <string>
 
 class Solution
