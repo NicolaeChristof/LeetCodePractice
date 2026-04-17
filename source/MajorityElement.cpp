35506007cd2e0aabbf371083f2db43accd852ec3
Nicolae Christoffersen <nicochristof@gmail.com>
2026-04-17 12:06:37 -0700

    Majority Element

---
+++ source/MajorityElement.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/majority-element
+#include <vector>
+
+#include <unordered_map>
+
+class Solution
+{
+public:
+    int majorityElement(std::vector<int>& nums)
+    {
+        std::unordered_map<int, int> numCount;
+
+        for (const int num : nums)
+        {
+            ++numCount[num];
+            if (numCount[num] > nums.size() / 2) return num;
+        }
+        return -1;
+    }
+};
