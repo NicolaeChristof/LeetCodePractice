e046a19877902d464c6b710fa7fb95eb56085eb4
Nicolae Christoffersen <nicochristof@gmail.com>
2026-04-18 11:54:12 -0700

    Contains Duplicates

---
+++ source/ContainsDuplicates.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/contains-duplicate
+#include <vector>
+
+#include <unordered_set>
+
+class Solution
+{
+public:
+    bool containsDuplicate(std::vector<int>& nums)
+    {
+        std::unordered_set<int> numSet;
+        for (const int num : nums)
+        {
+            if (numSet.contains(num)) return true;
+            numSet.insert(num);
+        }
+        return false;
+    }
+};
