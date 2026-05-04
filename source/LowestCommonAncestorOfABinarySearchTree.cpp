db431911547791722e9d9104bff779100aa48c70
Nicolae Christoffersen <nicochristof@gmail.com>
2026-05-04 16:20:50 -0700

    Fixed spacing

--- source/LowestCommonAncestorOfABinarySearchTree.cpp
+++ source/LowestCommonAncestorOfABinarySearchTree.cpp
@@ -7,16 +7,16 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
- class Solution
- {
- public:
-     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
-     {
-         if (root == nullptr || root == p || root == q) return root;
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (root == nullptr || root == p || root == q) return root;
 
-         if (p->val < root->val && q->val < root->val) return lowestCommonAncestor(root->left, p, q);
-         if (p->val > root->val && q->val > root->val) return lowestCommonAncestor(root->right, p, q);
-         
-         return root;
-     }
- };
+        if (p->val < root->val && q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+        if (p->val > root->val && q->val > root->val) return lowestCommonAncestor(root->right, p, q);
+        
+        return root;
+    }
+};
