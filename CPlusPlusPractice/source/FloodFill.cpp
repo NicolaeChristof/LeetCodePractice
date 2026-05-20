c2b561c51e7aa1922739d2ce77833d0984e32e78
Nicolae Christoffersen <nicochristof@gmail.com>
2026-05-20 14:27:18 -0700

    Flood Fill C# + BFS C++ solution

--- CPlusPlusPractice/source/FloodFill.cpp
+++ CPlusPlusPractice/source/FloodFill.cpp
@@ -1,36 +1,69 @@
 // https://leetcode.com/problems/flood-fill
 #include <vector>
+#include <queue>
 
+// BFS solution
 class Solution
 {
 public:
-    void ColorNeighbor(std::vector<std::vector<int>>& image, int sr, int sc, int newColor, int originalColor)
-    {
-        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size()) return;
-        if (image[sr][sc] == newColor) return;
-
-        if (image[sr][sc] == originalColor)
-        {
-            image[sr][sc] = newColor;
-
-            ColorNeighbor(image, sr - 1, sc, newColor, originalColor);
-            ColorNeighbor(image, sr + 1, sc, newColor, originalColor);
-            ColorNeighbor(image, sr, sc - 1, newColor, originalColor);
-            ColorNeighbor(image, sr, sc + 1, newColor, originalColor);
-        }
-    }
-
     std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image, int sr, int sc, int color)
     {
-        if (image.empty()) return image;
+        int originalColor = image[sr][sc];
+        if (originalColor == color) return image;
 
-        ColorNeighbor(image, sr - 1, sc, color, image[sr][sc]);
-        ColorNeighbor(image, sr + 1, sc, color, image[sr][sc]);
-        ColorNeighbor(image, sr, sc - 1, color, image[sr][sc]);
-        ColorNeighbor(image, sr, sc + 1, color, image[sr][sc]);
+        const int rows = image.size();
+        const int cols = image[0].size();
 
-        image[sr][sc] = color;
+        std::queue<std::pair<int, int>> q;
+        q.emplace(sr, sc);
+
+        while (!q.empty())
+        {
+            const auto [row, col] = q.front();
+            q.pop();
+            image[row][col] = color;
+
+            if (row - 1 >= 0 && image[row - 1][col] == originalColor) q.emplace(row - 1, col);
+            if (row + 1 < rows && image[row + 1][col] == originalColor) q.emplace(row + 1, col);
+            if (col - 1 >= 0 && image[row][col - 1] == originalColor) q.emplace(row, col - 1);
+            if (col + 1 < cols && image[row][col + 1] == originalColor) q.emplace(row, col + 1);
+        }
 
         return image;
     }
 };
+
+// Recursive solution
+//class Solution
+//{
+//public:
+//    void ColorNeighbor(std::vector<std::vector<int>>& image, int sr, int sc, int newColor, int originalColor)
+//    {
+//        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size()) return;
+//        if (image[sr][sc] == newColor) return;
+//
+//        if (image[sr][sc] == originalColor)
+//        {
+//            image[sr][sc] = newColor;
+//
+//            ColorNeighbor(image, sr - 1, sc, newColor, originalColor);
+//            ColorNeighbor(image, sr + 1, sc, newColor, originalColor);
+//            ColorNeighbor(image, sr, sc - 1, newColor, originalColor);
+//            ColorNeighbor(image, sr, sc + 1, newColor, originalColor);
+//        }
+//    }
+//
+//    std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image, int sr, int sc, int color)
+//    {
+//        if (image.empty()) return image;
+//
+//        ColorNeighbor(image, sr - 1, sc, color, image[sr][sc]);
+//        ColorNeighbor(image, sr + 1, sc, color, image[sr][sc]);
+//        ColorNeighbor(image, sr, sc - 1, color, image[sr][sc]);
+//        ColorNeighbor(image, sr, sc + 1, color, image[sr][sc]);
+//
+//        image[sr][sc] = color;
+//
+//        return image;
+//    }
+//};
---
+++ CSharpPractice/source/FloodFill.cs
@@ -0,0 +1,33 @@
+﻿// https://leetcode.com/problems/flood-fill
+using System.Collections.Generic;
+
+namespace LeetCodePractice.FloodFill
+{
+    public class Solution
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            int originalColor = image[sr][sc];
+            if (originalColor == color) return image;
+
+            int rows = image.Length;
+            int cols = image[0].Length;
+
+            Queue<(int, int)> queue = new Queue<(int, int)>();
+            queue.Enqueue((sr, sc));
+
+            while (queue.Count > 0)
+            {
+                var (row, col) = queue.Dequeue();
+                image[row][col] = color;
+
+                if (row - 1 >= 0 && image[row - 1][col] == originalColor) queue.Enqueue((row - 1, col));
+                if (row + 1 < rows && image[row + 1][col] == originalColor) queue.Enqueue((row + 1, col));
+                if (col - 1 >= 0 && image[row][col - 1] == originalColor) queue.Enqueue((row, col - 1));
+                if (col + 1 < cols && image[row][col + 1] == originalColor) queue.Enqueue((row, col + 1));
+            }
+
+            return image;
+        }
+    }
+}
