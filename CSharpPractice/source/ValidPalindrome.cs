df4f5c98a068bdcc4b5c31c489be2f2ff652fc12
Nicolae Christoffersen <nicochristof@gmail.com>
2026-05-20 12:36:50 -0700

    Valid Palindrome C#

---
+++ CSharpPractice/source/ValidPalindrome.cs
@@ -0,0 +1,32 @@
+﻿// https://leetcode.com/problems/valid-palindrome
+using System.Collections.Generic;
+
+namespace LeetCodePractice.ValidPalindrome
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (s.Length == 0 || s.Length == 1) return true;
+
+            int front = 0;
+            int back = s.Length - 1;
+
+            while (front < back)
+            {
+                while (front < s.Length && !char.IsLetterOrDigit(s[front])) ++front;
+                while (back >= 0 && !char.IsLetterOrDigit(s[back])) --back;
+
+                if (front < back && char.ToLower(s[front]) != char.ToLower(s[back]))
+                {
+                    return false;
+                }
+
+                ++front;
+                --back;
+            }
+
+            return true;
+        }
+    }
+}
