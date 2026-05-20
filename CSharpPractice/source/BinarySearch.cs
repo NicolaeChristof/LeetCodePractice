81bbc275c57f25c81037b2744d0aaad6cccc50e8
Nicolae Christoffersen <nicochristof@gmail.com>
2026-05-20 13:28:45 -0700

    Binary Search C#

---
+++ CSharpPractice/source/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿// https://leetcode.com/problems/binary-search
+
+namespace LeetCodePractice.BinarySearch
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            int mid;
+            while (low <= high)
+            {
+                mid = low + (high - low) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (target < nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
