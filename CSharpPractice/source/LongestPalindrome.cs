1d55cb57e411947e7468843e8490ce85acf8c63b
Nicolae Christoffersen <nicochristof@gmail.com>
2026-05-29 11:54:10 -0700

    Longest Palindrome C#

---
+++ CSharpPractice/source/LongestPalindrome.cs
@@ -0,0 +1,44 @@
+﻿// https://leetcode.com/problems/longest-palindrome
+using System.Collections.Generic;
+
+namespace LeetCodePractice.LongestPalindrome
+{
+    public class Solution
+    {
+        public int LongestPalindrome(string s)
+        {
+            if (s.Length == 0) return 0;
+            if (s.Length == 1) return 1;
+
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (letters.ContainsKey(c))
+                {
+                    ++letters[c];
+                }
+                else
+                {
+                    letters[c] = 1;
+                }
+            }
+
+            int total = 0;
+            bool odd = false;
+            foreach (var (key, value) in letters)
+            {
+                if (value % 2 == 0) // Even
+                {
+                    total += value;
+                }
+                else // Odd
+                {
+                    total += (value - 1);
+                    odd = true;
+                }
+            }
+            if (odd) ++total;
+            return total;
+        }
+    }
+}
