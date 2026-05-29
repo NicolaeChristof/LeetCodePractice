85730b8f54d3d80e90a26b377b4de97a95c2e06e
Nicolae Christoffersen <nicochristof@gmail.com>
2026-05-29 11:41:56 -0700

    Climbing Stairs C#

---
+++ CSharpPractice/source/ClimbingStairs.cs
@@ -0,0 +1,21 @@
+﻿// https://leetcode.com/problems/climbing-stairs
+
+namespace LeetCodePractice.ClimbingStairs
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            int curr = -1;
+            int prev1 = 1;
+            int prev2 = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                curr = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = curr;
+            }
+            return curr;
+        }
+    }
+}
